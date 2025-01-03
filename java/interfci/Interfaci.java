0fd14b4b73e2a7c37339ec3e510595fb80691d98
Jackson084 <jackson.alexandre@escolar.ifrn.edu.br>
2025-01-03 15:31:01 -0300

    aprendendo sobre botão
    criei um botão e modifiquei suas proporções

--- java/interfci/Interfaci.java
+++ java/interfci/Interfaci.java
@@ -1,5 +1,8 @@
 package interfci;
 
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Interfaci {
@@ -11,8 +14,15 @@ public class Interfaci {
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE); // finalizar programa quando a janela é fechada
         jFrame.setResizable(false); // impede ajuste da janela quando o programa foi execultado
         jFrame.setLocationRelativeTo(null); // local que a janela aparecerá, nesse caso, ela aparecera no meio
+        jFrame.setLayout(null);
 
-        
+        JButton jbutton = new JButton(" clique aqui"); // mensagem do botão
+        jbutton.setText("Botão"); // modificar mensagem
+        jbutton.setBounds(100, 200, 200, 90); // modifica o canto que o botão estara
+        jbutton.setFont(new Font("Arial", Font.ITALIC, 40)); // modifica a fonte o estio e o tamanho da mensagem dentrp dp botão
+        jbutton.setForeground(new Color(50, 50, 100)); // modifca a cor da letra
+        jbutton.setBackground(new Color(100, 20, 30)); // modifica a cor de fundo do botão
+        jFrame.add(jbutton);
     }
     
 }
